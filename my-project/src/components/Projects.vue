86ad838308c25c342c721163f02c9423a0fc4f96
TakehikoEsaka <take.pbfzbf302@gmail.com>
2020-09-05 20:00:46 +0900

    bootstrap-vueを使ったalertとtoggleの実装

--- my-project/src/components/Projects.vue
+++ my-project/src/components/Projects.vue
@@ -1,19 +1,50 @@
 <template>
+
   <div class="container">
-    <p v-if="errored" v-cloak>{{ error }}</p>
-    <p v-if="loading" v-cloak>Loading...</p>
+    <!-- bootstrap-vueはDOM要素として使えるので，その中にv-if記載可能 -->
+    <b-alert show variant="danger" v-if="errored" >{{ error }}</b-alert>
+
+    <!-- v-cloakつけると※Mustache バインディング中に{{}}が表示されなくなる -->
+    <!-- 定義されてないデータだとmustacheは無視してエラーださずに見る事ができる -->
+    <b-alert show variant="info" v-if="loading" v-cloak >Loading from {{ api_url }} ...</b-alert>
 
     <div v-else>
       <div v-for="_info in info" :key="_info.id">
         <h2>{{_info.id}} {{_info.name}}</h2>
-        <Milestone :project_id="_info.id"/>
-    </div>
-    </div>
+        <!-- 以下がcollapseの部分 -->
+            <b-button
+              :class="visible ? null : 'collapsed'"
+              :aria-expanded="visible ? 'true' : 'false'"
+              aria-controls="collapse-4"
+              @click="visible = !visible">
+              Toggle Collapse
+            </b-button>
+            <b-collapse id="collapse-4" v-model="visible" class="mt-2">
+              <b-card>
+                <Milestone :project_id="_info.id"/>
+              </b-card>
+            </b-collapse>
+      </div>
+      <!-- 以下がカレンダーの部分 -->
+      <b-row>
+        <b-col md="auto">
+          <b-calendar v-model="value" @context="onContext" locale="en-US"></b-calendar>
+        </b-col>
+        <b-col>
+          <p>Value: <b>'{{ value }}'</b></p>
+          <p class="mb-0">Context:</p>
+          <pre class="small">{{ context }}</pre>
+        </b-col>
+      </b-row>      
 
+    </div>
   </div>
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.css' // added
+import 'bootstrap-vue/dist/bootstrap-vue.css' // added
+
 import axios from 'axios'
 import Milestone from './Milestone.vue'
 
@@ -24,16 +55,25 @@ export default {
   },
   data () {
       return {
+          api_url: 'https://gitlab.com/api/v4/users/2904302/projects',
           info: null,
           loading: true,
           errored: false,
           error: null,
+          visible: true,
+          value: '',
+          context: null,
       }
+  },
+  methods : {
+    onContext(ctx) {
+      this.content = ctx
+    }
   },
   created () {
     // axios.defaults.withCredentials = true //これをつけると怒られてしまう
     axios
-    .get('https://gitlab.com/api/v4/users/2904302/projects', {
+    .get(this.api_url, { // dataから参照する時はthis.hogeでアクセス
       "params" : {
         sort: "asc"
       }
@@ -74,5 +114,9 @@ a {
 .container {
   text-align: -webkit-center;
   width: 100%;
+}
+.btn btn-primary {
+	position: absolute;
+	z-index: 100;
 }
 </style>
