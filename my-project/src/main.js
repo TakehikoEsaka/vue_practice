fbb7eca45c42e425fcfb2fccd92616545e3ba4a9
TakehikoEsaka <take.pbfzbf302@gmail.com>
2020-09-05 19:59:25 +0900

    bootstrap-vueの追加

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 以下はbootstrapを使うための書式
+import moment from 'vue-moment'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(moment)
+Vue.use(BootstrapVue)
+
 // Vue.config.productionTip = false
 
 // 以下でindex.htmlのid="app"にAppという名前でコンポーネントをマウント
